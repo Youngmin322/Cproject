e541f6b7747d44a57965ccb4541db3ed2bca536b
Youngmin322 <choyoungmin0322@gmail.com>
2024-05-26 16:50:43 +0900

    Splash code write

--- Cproject/SplashViewController.swift
+++ Cproject/SplashViewController.swift
@@ -8,22 +8,20 @@
 import UIKit
 
 class SplashViewController: UIViewController {
-
+    @IBOutlet weak var appIconCenterXConstraint: NSLayoutConstraint!
+    @IBOutlet weak var appIconCenterYConstraint: NSLayoutConstraint!
+    @IBOutlet weak var appIcon: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 1, animations:  { [weak self] in
+            let rotationAngle: CGFloat = CGFloat.pi
+            self?.appIcon.transform = CGAffineTransform(rotationAngle: rotationAngle)
+        })
     }
-    */
-
 }
